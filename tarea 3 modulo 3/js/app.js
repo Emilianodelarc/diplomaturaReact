38f99d64b2b678adc5db979ba6461aeed94f48fa
emilianodelarc <emilianodelarc@gmail.com>
2023-02-03 16:45:51 -0300

    agregado de console.log en remplazo de alert, como salida de datos

--- tarea 3 modulo 3/js/app.js
+++ tarea 3 modulo 3/js/app.js
@@ -1,6 +1,6 @@
 
 //EJERCICIO 1
- alert('bienvenido al sistema de calculo de distancia');
+// alert('bienvenido al sistema de calculo de distancia');
 
 for (let i = 2; i >= 0; i--) {
     alert('en caso de digitar mal Tienes ' + (i + 1) + ' oportunidades');
@@ -12,22 +12,22 @@ for (let i = 2; i >= 0; i--) {
     } else {
         if (distancia <= 1000) {
 
-            alert('a esa distacia deberia ir a PIE');
+            console.log('a esa distacia deberia ir a PIE');
 
         } else if(distancia >= 1001 && distancia <= 10000){
 
-            alert('a esa distacia deberia ir en BICICLETA');
+            console.log('a esa distacia deberia ir en BICICLETA');
 
         } else if(distancia >=10001  && distancia <= 30000){
 
-            alert('a esa distacia deberia tomar un COLECTIVO');
+            console.log('a esa distacia deberia tomar un COLECTIVO');
 
         } else if(distancia >= 30001 && distancia <= 100000){
 
-            alert('a esa distacia deberia ir en AUTO');
+            console.log('a esa distacia deberia ir en AUTO');
             
         }else{
-            alert('a esa distacia deberia ir en AVION');
+            console.log('a esa distacia deberia ir en AVION');
         }  
         break;
     }
@@ -35,14 +35,17 @@ for (let i = 2; i >= 0; i--) {
 
 //EJERCICIO 2
 
-let arrayNumeros = [1, 8, -5, 6, 50, 3, -50, 80]
+let arrayNumeros = [1, 8, 6, 50, 3 ,-5, -50, 80]
 let maximo = 0
 alert('ahora veremos que numero es mayor de una coleccion de numeros');
+console.log('los numeros recorridos son: ', arrayNumeros)
 
 for (let i = 0; i < arrayNumeros.length; i++) {
+
     if(maximo < arrayNumeros[i]){
         maximo = arrayNumeros[i]
     }
+
 }
 alert(`El numero mayor es ${maximo}`)
        
